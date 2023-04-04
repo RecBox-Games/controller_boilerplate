3a1946e741fe86e2e23c796009075eae73fd59e3
Weston Kelliher <westonkelliher@gmail.com>
2023-04-04 15:09:09 -0700

    can draw rectangles; options for centering text

--- index.js
+++ index.js
@@ -81,16 +81,6 @@ ws.onopen = (event) => {
         handleMessage(msg);
     });
 
-    function draw_dot(x, y, color) {
-        ctx.beginPath();
-        ctx.arc(x, y, 8, 0, 2*Math.PI);
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.lineWidth = 4;
-        ctx.strokeStyle = '#000011';
-        ctx.stroke();
-        console.log("drew");
-    }
 
     window.addEventListener("touchstart", (event) => {
         for (touch of event.changedTouches) {
@@ -127,22 +117,43 @@ ws.onopen = (event) => {
         ctx.setTransform(1,0,0,1,0,0);
     }
 
-    function draw_text(text, x, y, font, color) {
-        ctx.fillStyle = color;
+    function draw_text(text, x, y, font, color, centeredX, centeredY) {
         ctx.font = font;
+        if (centeredX) {
+            x -= ctx.measureText(text).width / 2;
+        }
+        if (centeredY) {
+            y += parseInt(ctx.font) / 3;
+        }
+        ctx.fillStyle = color;
         ctx.fillText(text, x, y);
+    }
+
+    function draw_rect(x, y, w, h, color, outline) {
+        if (outline == 0) {
+            ctx.fillStyle = color;
+            ctx.fillRect(x, y, w, h);
+        } else {
+            ctx.strokeStyle = color;
+            ctx.lineWidth = outline;
+            ctx.strokeRect(x, y, w, h);
+        }
     }
     
     // set defaults then call the appropriate draw function depending on the type
     function draw_drawable(drbl) {
+        if (! drbl) { console.log("none object for drawable");return; }
         if (! drbl.type) { console.log("no type for drawable");return; }
         if (drbl.type == 'text') {
             if (! drbl.text) { console.log("no text for text drawable");return; }
             if (! drbl.x) { drbl.x = 0; }
             if (! drbl.y) { drbl.y = 0; }
+            if (! drbl.centeredX) { drbl.centeredX = false; }
+            if (! drbl.centeredY) { drbl.centeredY = false; }
             if (! drbl.font) { drbl.font = '24px serif'; }
             if (! drbl.color) { drbl.color = '#000000'; }
-            draw_text(drbl.text, drbl.x, drbl.y, drbl.font, drbl.color);
+            draw_text(drbl.text, drbl.x, drbl.y, drbl.font, drbl.color,
+                      drbl.centeredX, drbl.centeredY);
         } else if (drbl.type == 'image') {
             if (! drbl.image) { console.log("no image for image drawable");return; }
             if (! drbl.image.complete) { return; }
@@ -151,11 +162,19 @@ ws.onopen = (event) => {
             if (! drbl.y) { drbl.y = 0; }
             if (! drbl.scaleX) { drbl.scaleX = 1; }            
             if (! drbl.scaleY) { drbl.scaleY = 1; }
-            if (! drbl.centerX) { drbl.centerX = -drbl.image.width / 2; }
-            if (! drbl.centerY) { drbl.centerY = -drbl.image.height / 2; }
+            if (! drbl.centeredX) { drbl.centeredX = -drbl.image.width / 2; }
+            if (! drbl.centeredY) { drbl.centeredY = -drbl.image.height / 2; }
             if (! drbl.rotation) { drbl.rotation = 0; }
             draw_image(drbl.image, drbl.x, drbl.y, drbl.scaleX, drbl.scaleY,
-                       drbl.centerX, drbl.centerY, drbl.rotation);
+                       drbl.centeredX, drbl.centeredY, drbl.rotation);
+        } else if (drbl.type == 'rect') {
+            if (! drbl.x) { drbl.x = 0; }
+            if (! drbl.y) { drbl.y = 0; }
+            if (! drbl.w) { drbl.x = 10; }
+            if (! drbl.h) { drbl.y = 10; }
+            if (! drbl.color) { drbl.color = '#000000'; }
+            if (! drbl.outline) { drbl.outline = 0; }            
+            draw_rect(drbl.x, drbl.y, drbl.w, drbl.h, drbl.color, drbl.outline);
         } else {
             console.log("Drawable type '" + drbl.type.toString() + "' not implemented");
         }
@@ -165,6 +184,7 @@ ws.onopen = (event) => {
         controlpadUpdate();
         let msgs = outgoingMessages();
         for (msg of msgs) {
+            console.log("sending <" + msg + ">");
             ws.send(msg);
         }
         let drbls = getDrawables();
@@ -172,7 +192,6 @@ ws.onopen = (event) => {
             ctx.fillStyle = "#808080";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
             for (drbl of drbls) {
-                
                 draw_drawable(drbl);
             }
         }
