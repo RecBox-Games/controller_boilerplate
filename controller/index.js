83a9822f23665d2d70c4abcf3a75ba99ef5b5ab5
FlavorlessQuark <flavorlessquark@gmail.com>
2023-06-10 16:17:15 -0700

    controller done

--- controller/index.js
+++ controller/index.js
@@ -38,12 +38,12 @@ const init_controller = () => {
     controller.enter.image = enter;
     controller.exit.image = exit;
     size_controller();
-    buttons_add(new Button(controller.enter.dst, undefined, undefined, () => { console.log("enter"); }));
-    buttons_add(new Button(controller.exit.dst, undefined, undefined, () => { console.log("exit"); }));
-    buttons_add(new Button(controller.up.dst, undefined, undefined, () => { console.log("up"); }));
-    buttons_add(new Button(controller.down.dst, undefined, undefined, () => { console.log("down"); }));
-    buttons_add(new Button(controller.left.dst, undefined, undefined, () => { console.log("left"); }));
-    buttons_add(new Button(controller.right.dst, undefined, undefined, () => { console.log("right"); }));
+    buttons_add(new Button(controller.enter.dst, undefined, undefined, () => { console.log("enter"); get_context().ws.send('select'); }));
+    buttons_add(new Button(controller.exit.dst, undefined, undefined, () => { console.log("exit"); get_context().ws.send('back'); }));
+    buttons_add(new Button(controller.up.dst, undefined, undefined, () => { console.log("up"); get_context().ws.send('up'); }));
+    buttons_add(new Button(controller.down.dst, undefined, undefined, () => { console.log("down"); get_context().ws.send('down'); }));
+    buttons_add(new Button(controller.left.dst, undefined, undefined, () => { console.log("left"); get_context().ws.send('left'); }));
+    buttons_add(new Button(controller.right.dst, undefined, undefined, () => { console.log("right"); get_context().ws.send('right'); }));
 };
 const size_landscape = () => {
     const dims = get_context().dimensions;
@@ -57,10 +57,24 @@ const size_landscape = () => {
     controller.down.dst = { ...controller.enter.dst, x: controller.enter.dst.x - 5, y: controller.enter.dst.y + controller.enter.dst.h * 0.8 + pad };
     controller.up.dst = { ...controller.enter.dst, x: controller.enter.dst.x + 5, y: controller.enter.dst.y - controller.enter.dst.h * 0.8 - pad };
 };
+const size_portrait = () => {
+    const dims = get_context().dimensions;
+    const pad = 25;
+    const enter_origin = { y: dims.y * 0.20, x: dims.x * 0.5 - dims.x * 0.125 };
+    const exit_origin = { y: dims.y - dims.y * 0.35, x: dims.x * 0.5 - dims.x * 0.25 };
+    controller.enter.dst = { y: enter_origin.y, x: enter_origin.x, w: dims.x * 0.29, h: dims.x * 0.29 };
+    controller.exit.dst = { y: exit_origin.y, x: exit_origin.x, w: dims.x * 0.5, h: dims.x * 0.5 };
+    controller.up.dst = { ...controller.enter.dst, x: controller.enter.dst.x + 6, y: controller.enter.dst.y - controller.enter.dst.h * 0.8 - pad };
+    controller.down.dst = { ...controller.enter.dst, x: controller.enter.dst.x - 6, y: controller.enter.dst.y + controller.enter.dst.h * 0.8 + pad };
+    controller.right.dst = { ...controller.enter.dst, y: controller.enter.dst.y - 0, x: controller.enter.dst.x + controller.enter.dst.w * 0.8 + pad };
+    controller.left.dst = { ...controller.enter.dst, y: controller.enter.dst.y + 0, x: controller.enter.dst.x - controller.enter.dst.w * 0.8 - pad };
+};
 const size_controller = () => {
     const ctx = get_context();
     if (ctx.dimensions.x > ctx.dimensions.y)
         size_landscape();
+    else
+        size_portrait();
 };
 const app = () => {
     const ctx = get_context();
--- index.ts
+++ index.ts
@@ -55,13 +55,12 @@ const init_controller = () => {
 	controller.exit.image = exit;
 
 	size_controller();
-
-	buttons_add(new Button(controller.enter.dst as Rectangle, undefined, undefined, () => {console.log("enter"); get_context().ws.send('')}))
-	buttons_add(new Button(controller.exit.dst as Rectangle, undefined, undefined, () => {console.log("exit")}))
-	buttons_add(new Button(controller.up.dst as Rectangle, undefined, undefined, () => {console.log("up")}))
-	buttons_add(new Button(controller.down.dst as Rectangle, undefined, undefined, () => {console.log("down")}))
-	buttons_add(new Button(controller.left.dst as Rectangle, undefined, undefined, () => {console.log("left")}))
-	buttons_add(new Button(controller.right.dst as Rectangle, undefined, undefined, () => {console.log("right")}))
+	buttons_add(new Button(controller.enter.dst as Rectangle, undefined, undefined, () => {console.log("enter"); get_context().ws.send('select')}))
+	buttons_add(new Button(controller.exit.dst as Rectangle, undefined, undefined, () => {console.log("exit"); get_context().ws.send('back')}))
+	buttons_add(new Button(controller.up.dst as Rectangle, undefined, undefined, () => {console.log("up"); get_context().ws.send('up')}))
+	buttons_add(new Button(controller.down.dst as Rectangle, undefined, undefined, () => {console.log("down"); get_context().ws.send('down')}))
+	buttons_add(new Button(controller.left.dst as Rectangle, undefined, undefined, () => {console.log("left"); get_context().ws.send('left')}))
+	buttons_add(new Button(controller.right.dst as Rectangle, undefined, undefined, () => {console.log("right"); get_context().ws.send('right')}))
 }
 
 const size_landscape = () => {
@@ -79,12 +78,30 @@ const size_landscape = () => {
 
 
 }
+const size_portrait = () => {
+	const dims = get_context().dimensions;
+	const pad = 25;
+	const enter_origin:Point  = {y: dims.y * 0.20 , x:dims.x * 0.5 - dims.x * 0.125};
+	const exit_origin:Point  = {y: dims.y - dims.y * 0.35 , x:dims.x * 0.5 - dims.x * 0.25 };
+
+	controller.enter.dst = {y: enter_origin.y, x: enter_origin.x, w: dims.x * 0.29, h: dims.x * 0.29};
+	controller.exit.dst = {y: exit_origin.y, x: exit_origin.x, w: dims.x * 0.5, h: dims.x * 0.5};
+
+	controller.up.dst = {...controller.enter.dst,  x: controller.enter.dst.x + 6, y: controller.enter.dst.y - controller.enter.dst.h * 0.8 - pad};
+	controller.down.dst = {...controller.enter.dst, x: controller.enter.dst.x - 6, y: controller.enter.dst.y + controller.enter.dst.h * 0.8 + pad};
+	controller.right.dst = {...controller.enter.dst,y: controller.enter.dst.y - 0, x: controller.enter.dst.x + controller.enter.dst.w * 0.8 + pad};
+	controller.left.dst = {...controller.enter.dst,y: controller.enter.dst.y + 0, x: controller.enter.dst.x - controller.enter.dst.w * 0.8 - pad};
+
+
+}
 
 const size_controller = () => {
 	const ctx = get_context();
 
 	if (ctx.dimensions.x > ctx.dimensions.y)
 		size_landscape();
+	else
+		size_portrait();
 }
 
 
